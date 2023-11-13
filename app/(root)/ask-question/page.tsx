eeb5d122df690e8b11177abd6bd34f56115c722c
Gabriel Zouad <z20gab@gmail.com>
2023-11-13 19:29:03 +0100

    Added so nextjs config can take images from external sources,fixed userId to be dynamic and not static

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,16 +1,14 @@
 import Question from '@/components/forms/Question';
 import { getUserByID } from '@/lib/actions/user.action';
-// import { auth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
 import React from 'react';
 import { redirect } from 'next/navigation';
 
 const Page = async () => {
-  // const { userId } = auth();
-  const userId = '12345';
+  const { userId } = auth();
   if (!userId) redirect('/sign-in');
 
   const mongoUser = await getUserByID({ userId });
-  console.log(mongoUser);
   return (
     <div>
       <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
