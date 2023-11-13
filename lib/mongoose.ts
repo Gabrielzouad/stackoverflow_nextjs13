638935d91e138c2cb65e2c2542552afc9f0f9b99
Gabriel Zouad <z20gab@gmail.com>
2023-11-13 14:16:01 +0100

    added failsafe for mongoose connection

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -13,7 +13,7 @@ export const connectToDatabase = async () => {
   }
   
   try {
-    await mongoose.connect(process.env.MONGODB_URL, 
+    await mongoose.connect(process.env.MONGODB_URL!, 
       { dbName:"devOverFlow"})
 
       isConnected = true;
